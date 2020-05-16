3588d5bc41dec1f17bd02c4721ea9f312ed9cfbd
takakd <takakd@takakd.com>
2020-05-16 17:04:54 +0900

    green note:change Money to interface

--- tddexercise/part1.go
+++ tddexercise/part1.go
@@ -1,33 +1,31 @@
 package tddexercise
 
 // Money
-type Money struct {
-	amount int
-}
-
-func (m Money)times(multipiler int) Money {
-	return Money{}
-}
-
-func (m Money)currency() string {
-	return ""
+type Money interface {
+	amount() int
+	times(multipiler int) Money
+	currency() string
 }
 
 func MakeDollar(amount int) Money {
-	return Dollar{Money{amount: amount}}.Money
+	return Dollar{amountValue: amount}
 }
 
 func MakeFranc(amount int) Money {
-	return Franc{Money{amount: amount}}.Money
+	return Franc{amountValue: amount}
 }
 
 // Dollar
 type Dollar struct {
-	Money
+	amountValue int
+}
+
+func (d Dollar) amount() int {
+	return d.amountValue
 }
 
 func (d Dollar) times(multiplier int) Money {
-	return MakeDollar(d.amount * multiplier)
+	return MakeDollar(d.amountValue * multiplier)
 }
 
 func (d Dollar) currency() string {
@@ -36,11 +34,15 @@ func (d Dollar) currency() string {
 
 // Franc
 type Franc struct {
-	Money
+	amountValue int
+}
+
+func (f Franc) amount() int {
+	return f.amountValue
 }
 
 func (f Franc) times(multiplier int) Money {
-	return MakeFranc(f.amount * multiplier)
+	return MakeFranc(f.amountValue * multiplier)
 }
 
 func (f Franc) currency() string {
--- tddexercise/part1_test.go
+++ tddexercise/part1_test.go
@@ -6,11 +6,11 @@ func Test_Multiplication(t *testing.T) {
 	five := MakeDollar(5)
 
 	if MakeDollar(10) != five.times(2) {
-		t.Errorf("expected=%d, actual=%d", five.times(2).amount, 10)
+		t.Errorf("expected=%d, actual=%d", five.times(2).amount(), 10)
 	}
 
 	if MakeDollar(15) != five.times(3) {
-		t.Errorf("expected=%d, actual=%d", five.times(3).amount, 15)
+		t.Errorf("expected=%d, actual=%d", five.times(3).amount(), 15)
 	}
 }
 
@@ -39,11 +39,11 @@ func Test_FrancMultiplication(t *testing.T) {
 	five := MakeFranc(5)
 
 	if MakeFranc(10) != five.times(2) {
-		t.Errorf("expected=%d, actual=%d", five.times(2).amount, 10)
+		t.Errorf("expected=%d, actual=%d", five.times(2).amount(), 10)
 	}
 
 	if MakeFranc(15) != five.times(3) {
-		t.Errorf("expected=%d, actual=%d", five.times(3).amount, 15)
+		t.Errorf("expected=%d, actual=%d", five.times(3).amount(), 15)
 	}
 }
 
