2f423bfb65222eea1579e24eb1c6122eba57bc1a
takakd <takakd@takakd.com>
2020-05-16 17:33:18 +0900

    green

--- tddexercise/part1.go
+++ tddexercise/part1.go
@@ -1,50 +1,60 @@
 package tddexercise
 
 // Money
-type Money interface {
+type IMoney interface {
 	amount() int
-	times(multiplier int) Money
+	times(multiplier int) IMoney
 	currency() string
 }
 
-func MakeDollar(amount int) Money {
-	return Dollar{amountValue: amount}
+func MakeDollar(amount int) IMoney {
+	return Dollar{Money{amountValue: amount, currencyValue: "USD"}}
 }
 
-func MakeFranc(amount int) Money {
-	return Franc{amountValue: amount}
+func MakeFranc(amount int) IMoney {
+	return Franc{Money{amountValue: amount, currencyValue: "CHF"}}
+}
+
+// common impl
+type Money struct {
+	amountValue   int
+	currencyValue string
+}
+
+func (m Money) amount() int {
+	return m.amountValue
+}
+
+func (m Money) currency() string {
+	return m.currencyValue
+}
+
+func (m Money) times(multiplier int) IMoney {
+	return m
 }
 
 // Dollar
 type Dollar struct {
-	amountValue int
+	Money
 }
 
 func (d Dollar) amount() int {
 	return d.amountValue
 }
 
-func (d Dollar) times(multiplier int) Money {
+func (d Dollar) times(multiplier int) IMoney {
 	return MakeDollar(d.amountValue * multiplier)
-}
-
-func (d Dollar) currency() string {
-	return "USD"
 }
 
 // Franc
 type Franc struct {
-	amountValue int
+	Money
 }
 
 func (f Franc) amount() int {
 	return f.amountValue
 }
 
-func (f Franc) times(multiplier int) Money {
+func (f Franc) times(multiplier int) IMoney {
 	return MakeFranc(f.amountValue * multiplier)
-}
-
-func (f Franc) currency() string {
-	return "CHF"
 }
