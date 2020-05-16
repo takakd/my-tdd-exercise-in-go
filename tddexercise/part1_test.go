3405257fe81b220f1f08ae498e9a7813c0f60ac6
takakd <takakd@takakd.com>
2020-05-16 14:21:48 +0900

    red

--- tddexercise/part1_test.go
+++ tddexercise/part1_test.go
@@ -16,3 +16,11 @@ func Test_Multiplication(t *testing.T) {
 	}
 }
 
+func Test_Equality(t *testing.T) {
+	if NewDollar(5) != NewDollar(5) {
+		t.Error("not equal.")
+	}
+	if NewDollar(5) == NewDollar(6) {
+		t.Error("not equal.")
+	}
+}
